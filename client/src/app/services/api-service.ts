93853ccb4eb75782abec839d2a5f4c7ae92972b2
Micah Norwood <micahnorwoodjordan@gmail.com>
2025-06-27 19:10:24 -0700

    add env settings module and api service to call zippopotam api

---
+++ client/src/app/services/api-service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from '../../environments/development';
+import { GeoData } from '../interfaces/GeoData';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  private env = environment;
+
+  public getZipCodeData(zipCode: string) {
+    let url: string = `${environment.baseURL}/${zipCode}`;
+    return this.httpClient.get<GeoData>(url, { responseType: 'json' });
+  }
+}
