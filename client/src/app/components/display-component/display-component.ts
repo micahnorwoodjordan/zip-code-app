b142e76718cf13c205ca40b3dcf855ea52f486b7
Micah Norwood <micahnorwoodjordan@gmail.com>
2025-06-27 19:31:39 -0700

    add input validation

--- client/src/app/components/display-component/display-component.ts
+++ client/src/app/components/display-component/display-component.ts
@@ -1,28 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgIf, AsyncPipe } from '@angular/common';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 
-import { FormsModule } from '@angular/forms';
+import {
+  FormControl, Validators, FormsModule, ReactiveFormsModule,
+  AbstractControl, ValidationErrors, ValidatorFn
+} from '@angular/forms';
+
+import { Observable, of } from 'rxjs';
 
 import { FlexLayoutModule } from 'ng-flex-layout';
+
+import { GeoData } from '../../interfaces/GeoData';
+import { ApiService } from '../../services/api-service';
 
 @Component({
   selector: 'app-display-component',
   imports: [
+    NgIf,
+    AsyncPipe,
     MatButtonModule,
     MatCardModule,
     MatInputModule,
     MatFormFieldModule,
     FormsModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    ReactiveFormsModule
   ],
   templateUrl: './display-component.html',
   styleUrl: './display-component.css'
 })
-export class DisplayComponent {
+export class DisplayComponent implements OnInit {
   public city: string = 'Phoenix';
   public state: string = 'Arizona';
+  public geoData$!: Observable<GeoData>;
+
+  public zipCodeInputFormControl = new FormControl('', [Validators.required, this.validateZipCodeInput()]);
+
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit(): void {
+    this.zipCodeInputFormControl.valueChanges.subscribe(value => {
+      // gets evaluated each time a user types into the form input field
+      if (!this.zipCodeErrorMessage && value !== null) {
+        console.log(value);
+      }
+    });
+    // this.apiService.getZipCodeData      
+  }
+
+  private validateZipCodeInput(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const isValid = /^\d{5}(-\d{4})?$/.test(control.value);
+      return isValid ? null : { invalidZipCode: true };
+    };
+  }
+
+  public get zipCodeErrorMessage(): string | null {
+    const control = this.zipCodeInputFormControl;
+    if (control.hasError('required')) {
+      return 'Zip code is required';
+    }
+    if (control.hasError('invalidZipCode')) {
+      return 'Please enter a valid United States zip code.';
+    }
+    return null;
+  }
 }
